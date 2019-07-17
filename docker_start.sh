5c8c0af55ad77406ba0cf6f7c3e147e2e65bfc74
Kei <39644776+keyskey@users.noreply.github.com>
2019-07-17 15:19:18 +0900

    Remove unnecessary comment

--- docker_start.sh
+++ docker_start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Overall Ref: https://qiita.com/succi0303/items/c74a969d0bda36df18de
-# Jupyter Docker Stacks official: https://jupyter-docker-stacks.readthedocs.io/en/latest/
-# build command: docker build -t jupyter/pyjulia-ml-notebook ./
-
 docker run \
   --rm \
   -p 8888:8888 \
